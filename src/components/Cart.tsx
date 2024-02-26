566159b76b42e4176e25d19d9d276c976ddc6b7e
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-02-26 17:26:09 +0400

    finish project implementation

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,8 +1,40 @@
+import useCart from "../hooks/useCart"
+import CartLineItem from "./CartLineItem"
+import { useState } from "react"
 
 function Cart() {
-  return (
-    <div>Cart</div>
-  )
+  const [confirm,setConfirm] = useState(false);
+  const {dispatch,REDUCER_ACTIONS,totalItems,totalPrice,cart} = useCart();
+
+  const onSubmitOrder = ()=>{
+    dispatch({type:REDUCER_ACTIONS.SUBMIT})
+    setConfirm(true);
+  }
+
+  const pageContent = confirm
+   ? <h2>Thank you for your order.</h2>
+   : <><h2 className="offscreen">Cart</h2>
+     <ul>
+        {cart.map(item=>{
+            return <CartLineItem key={item.sku}
+             item={item}
+             dispatch={dispatch}
+             REDUCER_ACTIONS={REDUCER_ACTIONS}
+             />
+        })}
+     </ul>
+     <div className="cart__totals">
+        <p>Total Items: {totalItems}</p>
+        <p>Total Price: {totalPrice}</p>
+        <button onClick={onSubmitOrder} className="cart__submit" disabled={!totalItems}>Place Order</button>
+     </div>
+    </>
+      const content = (
+        <main className="main main--cart">
+            {pageContent}
+        </main>
+      )
+  return content
 }
 
 export default Cart
---
+++ src/components/CartLineItem.tsx
@@ -0,0 +1,62 @@
+import { ChangeEvent, ReactElement } from "react"
+import { CartItemType } from "../context/CartProvider"
+import { ReducerAction } from "../context/CartProvider"
+import { ReducerActionType } from "../context/CartProvider"
+
+type propsType = {
+    item:CartItemType,
+    dispatch:React.Dispatch<ReducerAction>
+    REDUCER_ACTIONS:ReducerActionType
+}
+function CartLineItem({item,dispatch,REDUCER_ACTIONS}:propsType):ReactElement {
+    const img:string = new URL(`../images/${item.sku}.jpg`,import.meta.url).href
+
+    const lineTotal: number = (item.quantity * item.price);
+
+    const highestQuantity:number = 20 > item.quantity? 20:item.quantity
+
+    const optionValues:number[] = [...Array(highestQuantity).keys()].map(i=>i+1)
+
+    const options:ReactElement[] = optionValues.map(val=>{
+        return <option key={`opt${val}`} value={val}>{val}</option>
+    })
+
+    const onChangeQuantity = (e:ChangeEvent<HTMLSelectElement>) =>{
+        dispatch({
+            type:REDUCER_ACTIONS.QUANTITY,
+            payload:{...item,quantity:Number(e.target.value)}
+        })
+    }
+
+    const onRemoveFromCart = () =>{
+        dispatch({
+            type:REDUCER_ACTIONS.REMOVE,
+            payload: item
+        })
+    }
+    const content = (
+        <li className="cart__item">
+            <img src={img} alt={item.name} className="cart__img" />
+            <div aria-label="Item Name">{item.name}</div>
+            <div aria-label="Pricer Per Item">{new Intl.NumberFormat('en-US',{style:'currency',currency:'USD'}).format(item.price)}</div>
+
+            <label htmlFor="itemQty" className="offscreen">
+                Item Quantity
+            </label>
+            <select name="itemQty" id="itemQty" className="cart__select" value={item.quantity} aria-label="Item Quantity" onChange={onChangeQuantity}>{options}</select>
+            <div className="cart__item-subtotal" aria-label="Line-Item Subtotal">
+                {new Intl.NumberFormat('en-US',{style:'currency',currency:'USD'}).format(lineTotal)}
+            </div>
+            <button className="cart_button"
+              aria-label="Remove Item From Cart"
+              title="Remove Item From Cart"
+              onClick={(onRemoveFromCart)}
+            >
+                 &#10006;
+                             </button>
+        </li>
+    )
+  return content
+}
+
+export default CartLineItem
